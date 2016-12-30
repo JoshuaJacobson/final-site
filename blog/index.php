59eaa652a563fd3f70a74357e7ebd22c7b4b69db
Josh <fudbuds2@gmail.com>
2016-12-29 20:56:16 -0500

    fixed ourblog+connections

--- blog/index.php
+++ blog/index.php
@@ -22,20 +22,22 @@ $blog_editor = "Joshua Jacobson";
     <a href="/index.php">Return to homepage</a>
 </p>
 <hr>
-<div id="blog">
-    <h2>Our Blog</h2>
-    <p>
-        The Cadillac Connectors blog is a series of small updates and accompanying images that show our progress on all of our projects.
-        The blog is written by <?php echo $blog_writers ?> and edited by <?php echo $blog_editor ?>. During build season, expect nearly constant posts,
-        and in the off-season there may be as few as one per month.
-    </p>
-</div>
-<div id="newsletter">
-    <h2><i>Connections</i></h2>
-    <p>
-        <i>Connections</i> is Cadillac Connectors newsletter. During January, February, March, and April, an issue is released every two weeks.
-        During the rest of the year, an issue is released every month. To sign up for our newsletter, please <a href="articles/newsletter.php#signup">click here</a>.
-    </p>
+<div id="Container">
+    <div id="blog">
+        <h2>Our Blog</h2>
+        <p>
+            The Cadillac Connectors blog is a series of small updates and accompanying images that show our progress on all of our projects.
+            The blog is written by <?php echo $blog_writers ?> and edited by <?php echo $blog_editor ?>. During build season, expect nearly constant posts,
+            and in the off-season there may be as few as one per month.
+        </p>
+    </div>
+    <div id="newsletter">
+        <h2><i>Connections</i></h2>
+        <p>
+            <i>Connections</i> is Cadillac Connectors newsletter. During January, February, March, and April, an issue is released every two weeks.
+            During the rest of the year, an issue is released every month. To sign up for our newsletter, please <a href="articles/newsletter.php#signup">click here</a>.
+        </p>
+    </div>
 </div>
 </body>
 </html>
