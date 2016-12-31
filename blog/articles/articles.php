8ffe589f6ec8dc7a141eed2e843265ee00b61a3a
Josh <fudbuds2@gmail.com>
2016-12-31 12:48:36 -0500

    Finalizing articles class

--- blog/articles/articles.php
+++ blog/articles/articles.php
@@ -11,27 +11,43 @@ class articles
     static function getArticle($id) {
         //Add one to this number everytime a new article is created
         $articles = 0;
-        echo self::dateFormatter("05/16/2016", "humandate");
-        echo self::dateFormatter("05/16/2016", "date");
-        echo "05/16/2016";
 
         //Do not change this
         if($id == "latest") {
             $id = $articles;
         }
+        $author = "";
+        $date = "";
+        $title = "";
+        $paragraphs = new ArrayObject();
 
         //Add cases to this for every new article
         switch ($id) {
 
             //Article Number 0
             case 0:
-                echo "";
+                //Author's name
+                $author = "Joshua Jacobson";
+
+                //Date that article was published
+                $date = "12/31/2016";
+
+                //Title of article (should be less than 50 characters, doesn't have to be)
+                $title = "Old articles are going to be archived";
+
+                //Add paragraphs like this
+                $paragraphs->append("The old articles from the 2016 season are set to be archieved in  location to be determined.");
+                $paragraphs->append("They will be available shortly, please wait.");
+
                 break;
 
             //Article Number 1
             case 1:
                 break;
         }
+
+        //Do not change this
+        self::printArticle($author, self::dateFormatter($date, "date"), self::dateFormatter($date, "humandate"), $date, $title, $paragraphs);
     }
 
     static function printArticle($author, $date, $longdate, $humandate, $title, $paragraphs) {
